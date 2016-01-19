e59fac06ef61ec976ce5da2f411639418ab58862
Olga Botvinnik <olga.botvinnik@gmail.com>
2016-01-19 10:07:03 -0800

    add test for reading motifs

---
+++ kvector/tests/conftest.py
@@ -0,0 +1,11 @@
+
+import os
+import pytest
+
+@pytest.fixture(params=((3, 4), 3))
+def kmer_lengths(request):
+    return request.param
+
+@pytest.fixture
+def data_folder():
+    return os.path.join(os.path.abspath(os.path.dirname(__file__)), 'data')
--- kvector/tests/test_kmer.py
+++ kvector/tests/test_kmer.py
@@ -8,20 +8,10 @@ test_kmer
 Tests for `kvector.kmer` module.
 """
 
-import os
 import pandas as pd
 import pandas.util.testing as pdt
 import pytest
 import six
-
-
-@pytest.fixture(params=((3, 4), 3))
-def kmer_lengths(request):
-    return request.param
-
-@pytest.fixture
-def data_folder():
-    return os.path.join(os.path.abspath(os.path.dirname(__file__)), 'data')
 
 
 @pytest.fixture
