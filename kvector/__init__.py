d11ab8b35dae722b17b25d4f039f87e28590976c
Olga Botvinnik <olga.botvinnik@gmail.com>
2016-01-19 09:26:25 -0800

    remove create_metadata from export

--- kvector/__init__.py
+++ kvector/__init__.py
@@ -4,7 +4,7 @@ __author__ = 'Olga Botvinnik'
 __email__ = 'olga.botvinnik@gmail.com'
 __version__ = '0.1.0'
 
-from .io import read_motifs, create_metadata, motifs_to_kmer_vectors
+from .io import read_motifs, motifs_to_kmer_vectors
 from .kmer import count_kmers, score_kmers, make_kmers
 
 __all__ = ['read_motifs', 'create_metadata', 'motifs_to_kmer_vectors',
