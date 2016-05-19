c6d07a64af0b2fbda101f9347d66c3c40917a38e
Olga Botvinnik <olga.botvinnik@gmail.com>
2016-05-19 13:14:55 -0700

    Export per_interval_kmers to top-level namespace

--- kvector/__init__.py
+++ kvector/__init__.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 
 from .io import read_motifs, motifs_to_kmer_vectors
-from .kmer import count_kmers, score_kmers, make_kmers
+from .kmer import count_kmers, score_kmers, make_kmers, per_interval_kmers
 
 __author__ = 'Olga Botvinnik'
 __email__ = 'olga.botvinnik@gmail.com'
 __version__ = '0.1.0'
 
 __all__ = ['read_motifs', 'create_metadata', 'motifs_to_kmer_vectors',
-           'count_kmers', 'score_kmers', 'make_kmers']
+           'count_kmers', 'score_kmers', 'make_kmers', 'per_interval_kmers']
