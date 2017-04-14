95d4906a13986e02b3bf8e1fd7b6f0b477cfc93b
Olga Botvinnik <olga.botvinnik@gmail.com>
2017-04-14 13:46:31 -0700

    Version bump

--- kvector/__init__.py
+++ kvector/__init__.py
@@ -5,7 +5,7 @@ from .kmer import count_kmers, score_kmers, make_kmers, per_interval_kmers
 
 __author__ = 'Olga Botvinnik'
 __email__ = 'olga.botvinnik@gmail.com'
-__version__ = '0.1.0'
+__version__ = '1.0.0'
 
 __all__ = ['read_motifs', 'create_metadata', 'motifs_to_kmer_vectors',
            'count_kmers', 'score_kmers', 'make_kmers', 'per_interval_kmers']
